6c9f5e0b02be9274463a5529e1da00cd5e16e5eb
seagetch <sigetch@gmail.com>
2022-05-22 13:04:29 +0900

    Added initial support for J!NS MEME Logger (Wearable smart glasses)

--- source/ft/adaptors/package.d
+++ source/ft/adaptors/package.d
@@ -3,3 +3,4 @@ import ft.adaptor;
 public import ft.adaptors.vmc : VMCAdaptor;
 public import ft.adaptors.vtsproto : VTSAdaptor;
 public import ft.adaptors.openseeface : OSFAdaptor;
+public import ft.adaptors.jinsmemelogger : JMLAdaptor;
